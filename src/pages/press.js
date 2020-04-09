8737f16eb22e9282496f7de03df697f8d0e560d6
Jane <jane.e.nath@gmail.com>
2020-04-09 13:27:23 -0700

    render press quotes

--- src/pages/press.js
+++ src/pages/press.js
@@ -1,15 +1,55 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const press = () => (
-  <Layout>
-    <SEO title="Elias Miller Press" />
-    <h1>Press</h1>
+  <StaticQuery
+  query={graphql`
+    query {
+      allContentfulPress(sort: { order: DESC, fields: [date]}) {
+        edges {
+          node {
+            pressQuote {
+              childMarkdownRemark {
+                html
+              }
+            }
+            date(formatString: "MMMM Do, YYYY")
+            publication
+          }
+        }
+      }
+    }
+  `}
 
-  </Layout>
+  render={data => 
+    (<Layout>
+      <SEO title="Elias Miller Press" />
+      <h1>Press</h1>
+      
+        {data.allContentfulPress.edges.map(({ node }) =>(
+          <div className="reviewContainer">
+            <h3 dangerouslySetInnerHTML={{
+              __html: node.publication,
+            }}/>
+            <h4 dangerouslySetInnerHTML={{
+                __html: node.date,
+              }}/>
+            <p dangerouslySetInnerHTML={{
+                __html: node.pressQuote.childMarkdownRemark.html,
+              }}/>
+          </div>
+        )
+        )}
+        
+      
+    </Layout>
+    )} 
+
+  />
+
 )
 
 export default press
