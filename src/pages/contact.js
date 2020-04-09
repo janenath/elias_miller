9040d853f5e3cc5a149b66699e576ac9f533fa83
Jane <jane.e.nath@gmail.com>
2020-04-09 13:36:36 -0700

    add email link to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const contact = () => (
-  <Layout>
-    <SEO title="Contact Elias Miller" />
-    <h1>Contact Elias Miller</h1>
+  <StaticQuery
+    query={graphql`
+      query {
+        allContentfulContact {
+          edges {
+            node {
+              email
+            }
+          }
+        }
+      }
+    `}
 
-  </Layout>
+  render={data => (
+    <Layout>
+      <SEO title="Contact Elias Miller" />
+      <h1>Contact</h1>
+      <a href={`mailto:${data.allContentfulContact.edges[0].node.email}`}>email Elias Miller</a>
+    </Layout>
+  )}
+  />
 )
 
 export default contact
