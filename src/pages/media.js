7d536dc0797eef30083fc8626459dc9d9b1e63ee
Jane <jane.e.nath@gmail.com>
2020-04-29 10:46:07 -0700

    fix order query

--- src/pages/media.js
+++ src/pages/media.js
@@ -8,7 +8,7 @@ const recordings = () => (
   <StaticQuery
   query={graphql`
     query {
-      allContentfulMedia(sort: { order: ASC, fields: [order]}) {
+      allContentfulMedia(sort: { order: ASC }) {
         edges {
           node {
             title
