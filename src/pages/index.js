a80165a95082af5e593d13aa358ad666da336497
Jane <jane.e.nath@gmail.com>
2020-04-29 21:22:22 -0700

    fetch index text from contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,34 @@ import SEO from "../components/seo"
 import Headshot from "../images/headshot.png"
 
 const IndexPage = () => (
-
+    <StaticQuery
+    query={graphql`  
+    query {
+        contentfulHomeText {
+            text {
+              childMarkdownRemark {
+                html
+              }
+            }
+          }
+        }
+    `}
+    render={data => (
     <Layout>
     <SEO title="Home" />
     <div className="indexContainer">
         <img src={Headshot} alt="Elias Miller" className="headshot" width="40%"/>
         <div className="summaryContainer">
-        <p className="bioSummary">Music director of the Apollo Ensemble of Boston since 2018, Elias Miller has established a reputation as one of Boston's leading young conductors and orchestra builders.</p>
-        <br/><br/>
-        <p className="bioSummary bioQuote">"Throughout the evening, conductor Elias Miller achieved a fine balance of the components of his ensemble, bringing out the passionate violins and soaring flutes that often mirror soloists’ melismatic runs"</p>
-        <br/>
-        <p className="bioSummary bioQuote"><a href="https://www.classical-scene.com/2017/10/30/new-st-john/"> -The Boston Musical Intelligencer</a></p>
+        <p className="bioSummary" dangerouslySetInnerHTML={{
+            __html: data.contentfulHomeText.text.childMarkdownRemark.html,
+        }}/>
         </div>
     </div>
     </Layout>
+    )} 
 
+/>
+  
 )
 
 export default IndexPage
