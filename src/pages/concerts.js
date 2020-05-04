b901a174eb4534b9ed6213e35bc07e0a692887ac
Jane <jane.e.nath@gmail.com>
2020-05-04 11:17:59 -0700

    previous concert section added

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -20,6 +20,7 @@ const concerts = () => (
                   html
                 }
               }
+              past
               location
               description {
                 childMarkdownRemark {
@@ -34,38 +35,78 @@ const concerts = () => (
 
   render={data => (
   <Layout>
-    <SEO title="Elias Miller Concerts" />
     <div className="section">
+    <SEO title="Elias Miller Concerts" />
     <h1 className="title">Upcoming Concerts</h1>
-    <div className="section boxed">
-    {data.allContentfulEvent.edges.map(({ node }) =>(
-      <div className="schedule">
-        <h3><a href={node.link} target="_blank">{node.title}</a></h3>
-        <p>{node.role}</p>
-        <br/>
-        <p dangerouslySetInnerHTML={{
-          __html: node.dates.childMarkdownRemark.html,
-          }}/>
-        {node.location && 
-        <div>
-          <br/>
-          <p>{node.location}</p>
-        </div>
-        }
-        {node.description && 
-        <div>
-          <br/>
-          <p dangerouslySetInnerHTML={{
-            __html: node.description.childMarkdownRemark.html,
+    {data.allContentfulEvent.edges.map(({ node }) => (
+          <div className="section boxed">
+            {node.past == false &&
+            <div className="schedule">
+            <h3><a href={node.link} target="_blank">{node.title}</a></h3>
+            <p>{node.role}</p>
+            <br/>
+            <p dangerouslySetInnerHTML={{
+              __html: node.dates.childMarkdownRemark.html,
             }}/>
-        </div>
-        }
-      </div>
-)
-)}
-</div>
+            {node.location && 
+              <div>
+                <br/>
+                <p>{node.location}</p>
+              </div>
+            }
+            {node.description && 
+              <div>
+                <br/>
+                <p dangerouslySetInnerHTML={{
+                  __html: node.description.childMarkdownRemark.html,
+                }}/>
+              </div>
+
+             }
+              </div> 
+             }
+          </div>
+
+        ) 
+ 
+    )
+  }
+    <h1 className="title">Previous Concerts</h1>
+    {data.allContentfulEvent.edges.map(({ node }) => (
+          <div className="section boxed">
+            {node.past == true &&
+            <div className="schedule">
+            <h3><a href={node.link} target="_blank">{node.title}</a></h3>
+            <p>{node.role}</p>
+            <br/>
+            <p dangerouslySetInnerHTML={{
+              __html: node.dates.childMarkdownRemark.html,
+            }}/>
+            {node.location && 
+              <div>
+                <br/>
+                <p>{node.location}</p>
+              </div>
+            }
+            {node.description && 
+              <div>
+                <br/>
+                <p dangerouslySetInnerHTML={{
+                  __html: node.description.childMarkdownRemark.html,
+                }}/>
+              </div>
+
+             }
+              </div> 
+             }
+          </div>
+
+        ) 
+ 
+    )
+  }
   </div>
-  </Layout>
+</Layout>
 )}
 />
 )
